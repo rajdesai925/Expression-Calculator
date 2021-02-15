b486b1bff3865b7d67420c37f95e906e644f28f5
rajdesai925 <59585014+rajdesai925@users.noreply.github.com>
2021-02-15 17:08:52 -0500

    Update Variable.java

--- src/app/Variable.java
+++ src/app/Variable.java
@@ -3,9 +3,6 @@ package app;
 /**
  * This class holds a (name, integer value) pair for a simple (non-array) variable. 
  * The variable name is a sequence of one or more letters.
- * 
- * @author ru-nb-cs112
- *
  */
 public class Variable {
 	
